bafd52c7f7e71bee3253e2769fdaffe09589a138
chie-hira <capella.a36@gmail.com>
2024-08-19 16:09:05 +0900

    apiを叩いてデータを表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks'
+import { Book } from './types'
+
+const BOOKS = gql`
+  {
+    books {
+      title
+      author
+    }
+  }
+`
+
+// console.log(BOOKS);
+// この状態だと複雑な階層構造
+
+function Books() {
+  const { loading, error, data } = useQuery(BOOKS);
+  // console.log(data);
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <>Error: {error.message}</>
+  
+  return data.books.map(({title, author}: Book) => (
+    <div key={title}>
+      <p>
+        {title}: {author}
+      </p>
+    </div>
+  ))
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h2>GraphQL Client</h2>
+      <Books />
     </>
   )
 }
