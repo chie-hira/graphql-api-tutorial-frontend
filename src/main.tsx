9b32e06f85a1a9a97cea6ffc7aae35027d9412e5
chie-hira <capella.a36@gmail.com>
2024-08-19 15:46:35 +0900

    必要なライブラリインストール
    npm i @apollo/client @apollo/react-hooks graphql apollo-boost

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(), // 必須
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 )
